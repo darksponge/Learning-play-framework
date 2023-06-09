bea4416f9bed841b3674db0b6de594299aa3a9ab
Paul <moi@paultissot.ch>
2023-06-09 22:39:42 +0200

    Wrote a test for the view

--- server/test/TaskList1Spec.scala
+++ server/test/TaskList1Spec.scala
@@ -1,8 +1,27 @@
 import org.scalatestplus.play.{HtmlUnitFactory, OneBrowserPerSuite, PlaySpec}
 import org.scalatestplus.play.guice.GuiceOneServerPerSuite
+import play.api.Logger
+
 
 class TaskList1Spec extends PlaySpec with GuiceOneServerPerSuite with OneBrowserPerSuite with HtmlUnitFactory {
   "Task list 1" must {
-
+    "login and access functions" in {
+      go to s"http://localhost:$port/login1"
+//      println(pageTitle)
+      pageTitle mustBe "Login"
+      find(cssSelector("h2")).isEmpty mustBe false
+      find(cssSelector("h2")).foreach(_.text mustBe "Login")
+      click on "username-login"
+      textField("username-login").value = "Paul"
+      click on "password-login"
+      pwdField(id("password-login")).value = "12345678"
+      submit()
+      eventually {
+        pageTitle mustBe "Task List"
+        find(cssSelector("h2")).isEmpty mustBe false
+        find(cssSelector("h2")).foreach(_.text mustBe "Task List")
+        findAll(cssSelector("li")).toList.map(_.text) mustBe List("Make Videos", "Eat Bread", "Sleep", "Implement a Task List")
+      }
+    }
   }
 }
